8d3da6da16bfd1b7f9ab54a8d18d34b88ec1d4fe
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-06-12 23:14:00 +0100

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -29,3 +29,64 @@ Matrix<T> Matrix<T>::operator += (const Matrix& B){
     }
     return *this;
 }
+
+template<class T>
+Matrix<T> Matrix<T>::operator-=(const Matrix& B){
+            if(this -> n != B.m || this -> m != B.n){
+                throw std::invalid_argument("Matrices are incompatible for subtraction");
+            }
+            else{
+                for(int i = 0; i < this -> m; i++){
+                    for(int j = 0;  j < this -> n; j++){
+                        *this.data[i][j] -= B[i][j];
+                    }
+                }
+            }
+            return *this;
+        }
+
+template<class T>
+Matrix<T> Matrix<T>::operator*=(const double lambda){
+    for(int i = 0; i < this -> m; i++){
+        *this -> data[i] *= lambda;
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator = (const Matrix& B){
+    *this.data.resize(B.data.size(), 0)
+    for(int i = 0; i < this -> m; i++){
+        *this.data[i] = B.data[i];
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator == (const Matrix& B){
+    return *this == B;
+}
+
+// Member functions
+template<class T>
+double Matrix<T>::trace(){ // Function to find trace.
+        double ans = 0;
+        if(this -> n != A.n){
+            throw std::invalid_argument("Matrix is not square");
+        }
+        for(int i = 0. j = 0; i < this -> m && j < this -> m; i++, j++){
+            ans += *this[i][j];
+        }
+        return ans;
+    }
+
+template<class T>
+    Matrix<T> Matrix<T>::T(){ // Transpose function.
+        std::vector<T> temp(*this.m * *this.n, 0);
+        // Load the temp vector.
+        for(int i = 0; i < *this.m; i++){
+            for(int j = 0; j < *this.n; j++){
+                temp[*this.m*i + j] = *this.data[i][j];
+            }
+        }
+    }
